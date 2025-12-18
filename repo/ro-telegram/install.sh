f5974eedccb12880c73234f63ed6ee9c8fa23310
d34012 <findmake313@gmail.com>
2025-12-18 17:37:56 +1200

    Repo init

---
+++ repo/ro-telegram/install.sh
@@ -0,0 +1,6 @@
+tar -xf ./tsetup.6.2.3.tar.xz -C /opt/
+cp ./icon.png /opt/Telegram/
+cp ./Telegram.desktop /usr/local/share/applications/
+
+chmod 0755 /usr/local/share/applications/Telegram.desktop
+chmod 0755 /opt/Telegram/ -R
